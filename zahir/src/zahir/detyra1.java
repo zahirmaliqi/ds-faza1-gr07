6a0d912fdec3971d4dcd552896365bad530cd4e8
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-03-13 23:00:59 +0100

    Update detyra1.java

--- zahir/src/zahir/detyra1.java
+++ zahir/src/zahir/detyra1.java
@@ -18,6 +18,24 @@ public class detyra1 {
                count++;    
        }  
        System.out.println("Total:"+count);
+	char[] str=s.toCharArray();               
+       int sz=str.length;
+       int i=0,j=0,count1=0;
+       for( i =0;i<sz;i++) {
+    	   count1=0;
+    	   for(j =0;j<sz;j++)
+    	   {
+    	        if(j <i && str[i] == str[j]) {
+    			   break;}
+		   
+    		 if(str[j] == str[i] ) {
+    			   count1++;}
+    		    
+    		  if(j==sz-1) {
+    			   
+    			   System.out.println(str[i]); }
+    	   }
+                           }
        sc.close();
        }
 }
