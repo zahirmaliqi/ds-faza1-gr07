2f350ac514b026a45a597bdec8265f808c738989
lenovo <lenovo@192.168.0.175>
2020-03-13 01:04:16 +0100

    Count

---
+++ zahir/src/zahir/detyra1.java
@@ -0,0 +1,32 @@
+package zahir;
+
+import java.util.Scanner;
+
+public class detyra1 {
+
+	public static void main(String[] args) {
+		String s; 
+		Scanner sc = new Scanner(System.in);
+ 		System.out.print("Stringu:"); 
+ 		 
+ 		s = sc.nextLine();  
+ 	   int count = 0;    
+       
+       //Numeron totalin e karakterve perveq hapesires   
+       for(int i = 0; i < s.length(); i++) {    
+           if(s.charAt(i) != ' ')    
+               count++;    
+       }  
+       System.out.println("Total:"+count);
+       sc.close();
+       }
+       
+	
+		
+	}
+	
+	
+
+
+
+
