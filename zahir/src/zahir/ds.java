bdcc27695fcfbd184872bf679346f3cb911b8dce
natyravitija <53190322+natyravitija@users.noreply.github.com>
2020-03-29 16:46:58 +0200

    Update ds.java

--- zahir/src/zahir/ds.java
+++ zahir/src/zahir/ds.java
@@ -9,6 +9,16 @@
 	            System.out.println("Vlerat e lejuara jane [frequency,four-square,vigenere]");
 	            System.exit(0);
 	        }
+		      if (args[0].equals("frequency")) {
+
+            if(args.length!=2 || args[1].equals("")){
+                System.out.println("Argumenti i dyte duhet te jete mesazhi me gjatesi>0");
+                System.exit(0);
+            }
+            FrequencyInspector frequencyInspector = new FrequencyInspector(args[1]);
+            frequencyInspector.fillHashMap();
+
+        }
 	
 	            }
 	
