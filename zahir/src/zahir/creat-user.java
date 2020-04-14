8e631f912eed11a85ace42a2bfeaccf426c23b1d
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-04-14 21:41:05 +0200

    Create creat-user.java
    Creat-user komande

---
+++ zahir/src/zahir/creat-user.java
@@ -0,0 +1,32 @@
+public class creat-user{
+	
+    private final String emri;
+  
+
+    
+    public creat-user(String emri) {
+    
+        this.emri=emri;
+       
+    }
+
+	public  void file() {
+		
+		File stockFile = new File("C:\\Users\\lenovo\\siguria\\"+emri+".xml");
+		File stockFile1 = new File("C:\\Users\\lenovo\\siguria\\"+emri+".pub.xml");
+
+		try {
+		    stockFile.createNewFile();
+		    stockFile1.createNewFile();
+		} catch (IOException ioe) {
+		     System.out.println("Error" + ioe);
+		}
+
+		System.out.println("Eshte krijuar celsi privat \'keys/" + stockFile.getPath()+"\'");
+		;
+		System.out.println("Eshte krijuar celsi publik \'keys/" + stockFile1.getPath()+"\'");
+		;
+
+		}
+
+}
