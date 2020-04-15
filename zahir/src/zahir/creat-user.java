c217e64681146adbf75bd12c057f39bfc2cf3cce
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-04-15 21:58:24 +0200

    Update creat-user.java

--- zahir/src/zahir/creat-user.java
+++ zahir/src/zahir/creat-user.java
@@ -3,13 +3,13 @@ import java.io.*;
 
 public class creat-user{
 	
-    private final String emri;
+    private final String emri_key;
   
 
     
-    public creat-user(String emri) {
+    public creat-user(String emri_key) {
     
-        this.emri=emri;
+        this.emri_key=emri_key;
        
     }
 
