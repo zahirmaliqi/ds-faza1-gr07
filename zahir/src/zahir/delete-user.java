70dd0e836677dfc9a429e8a9c1f2b62caf6952f9
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-04-18 21:02:42 +0200

    Update delete-user.java

--- zahir/src/zahir/delete-user.java
+++ zahir/src/zahir/delete-user.java
@@ -1,1 +1,30 @@
+import java.io.File;
 
+public class delete-user{
+    private final String emri_key;
+   
+    public delete-user(String emri_key) {
+        this.emri_key=emri_key;
+    }
+
+
+    public void delete_file()throws Exception{	
+    	try{
+    		
+    		File file = new File("C:\\Users\\lenovo\\eclipse-workspace\\final\\"+emri_key+".xml");
+    		
+        	
+    		if(file.delete()){
+    			System.out.println(file.getName() + " is deleted!");
+    		}else{
+    			System.out.println("Delete operation is failed.");
+    		}
+    	   
+    	}catch(Exception e){
+    		
+    		e.printStackTrace();
+    		
+    	}
+    	
+    }
+}
