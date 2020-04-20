a079c18b17548d32ef94b5f3de68cde9480f8e49
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-04-20 23:53:43 +0200

    Update export-key.java

--- zahir/src/zahir/export-key.java
+++ zahir/src/zahir/export-key.java
@@ -1,1 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+public class excport{
+    private final String emri_key;
+   
+    public excport(String emri_key) {
+        this.emri_key=emri_key;
+    }
 
+
+
+    public  void mungon_argumentipub() throws ParserConfigurationException, SAXException, IOException {
+        
+        File xmlFile = new File("C:\\Users\\lenovo\\eclipse-workspace\\final\\"+emri_key+".pub.xml");
+       
+        
+        if (xmlFile.exists()) {
+        Reader fileReader = new FileReader(xmlFile);
+        BufferedReader bufReader = new BufferedReader(fileReader);
+        
+        StringBuilder sb = new StringBuilder();
+        String line = bufReader.readLine();
+        while( line != null){
+            sb.append(line).append("\n");
+            line = bufReader.readLine();
+        }
+        String xml2String = sb.toString();
+        System.out.println(xml2String);
+        
+        bufReader.close();}
+    
+    
+    }
+       
+              
+}
