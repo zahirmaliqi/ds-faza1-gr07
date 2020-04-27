5e3d3800e45f9ac3dceb0061117afeddecaa2903
tonikrasniqi <51675513+tonikrasniqi@users.noreply.github.com>
2020-04-27 23:19:20 +0200

    Import-key komanda
    Ne rastin kur args[2] ka mbaresen .pub.xml importohet ne file.xml qe e kemi japur ne args[1].

--- zahir/src/zahir/import-key.java
+++ zahir/src/zahir/import-key.java
@@ -11,3 +11,42 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
+
+public class import1{
+    private final String emri_key;
+    private final String emri_i_file;
+    public import1(String emri_key,String emri_i_file) {
+        this.emri_key=emri_key;
+        this.emri_i_file=emri_i_file;
+    }
+
+    public  void importkeypub() throws IOException {
+		
+		File dir1 = new File("C:\\Users\\lenovo\\eclipse-workspace\\final");
+ 
+		
+		String source = dir1.getCanonicalPath() + File.separator + emri_i_file;
+		String dest = dir1.getCanonicalPath() + File.separator + emri_key+".pub.xml";
+ 
+		File fin = new File(source);
+		FileInputStream fis = new FileInputStream(fin);
+		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+ 
+		FileWriter fstream = new FileWriter(dest, true);
+		BufferedWriter out = new BufferedWriter(fstream);
+ 
+		String aLine = null;
+		while ((aLine = in.readLine()) != null) {
+			
+			out.write(aLine);
+			out.newLine();
+		}
+ 
+		
+		in.close();
+		out.close();
+		System.out.println("Celesi publik u ruajt ne fajllin \'keys/"+emri_i_file+".pub.xml\'.");
+	}
+    
+
+}
