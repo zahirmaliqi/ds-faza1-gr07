d538e1f8593e7908b9c8fea87502a549f4b4450c
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-05-03 23:12:23 +0200

    Write-message
    Gjenerimi i emrit te perdoruesit ne base 64 dhe nje vlere <iv> me vlere 8 bajte te rastesishme

--- zahir/src/zahir/write-message.java
+++ zahir/src/zahir/write-message.java
@@ -1,6 +1,13 @@
 package zahir;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Base64;
 import java.util.Random;
@@ -14,29 +21,58 @@ public class writemessage{
 	
     private final String emri_i_perdoruesit;
     private final String text;
+    private final String file;
 
     
-    public writemessage(String emri_i_perdoruesit, String text) {
+    public writemessage(String emri_i_perdoruesit, String text, String file) {
     	
         this.emri_i_perdoruesit=emri_i_perdoruesit;
         this.text=text;
+	this.file=file;
     }
     public void enc() throws Exception {
     	String originalInput = emri_i_perdoruesit;
     	String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
-    	
+	File xmlFile = new File("C:\\Users\\lenovo\\eclipse-workspace\\final\\"+emri_i_perdoruesit+".pub.xml");
+	    
+    	if (xmlFile.exists()) {
     	Random random = ThreadLocalRandom.current();
-		byte[] randomBytes = new byte[8];
-		random.nextBytes(randomBytes);
-		String encoded = Base64.getUrlEncoder().encodeToString(randomBytes);
-        System.out.print(encodedString+"."+encoded);
+	byte[] randomBytes = new byte[8];
+	random.nextBytes(randomBytes);
+	String encoded = Base64.getUrlEncoder().encodeToString(randomBytes);
+	Random random1 = ThreadLocalRandom.current();
+	byte[] randomBytes1 = new byte[8];
+	random1.nextBytes(randomBytes1);
+	String key1= Base64.getUrlEncoder().encodeToString(randomBytes1);
+        String ciphertext = encrypt(key1,text);
+        String decrypted = decrypt(key1, ciphertext.trim());
+        String encrypted = encrypt(key1, decrypted.trim());
+        Reader fileReader = new FileReader(xmlFile);
+        BufferedReader bufReader = new BufferedReader(fileReader);
+        
+        StringBuilder sb = new StringBuilder();
+        String line = bufReader.readLine();
+        while( line != null){
+            sb.append(line).append("");
+            line = bufReader.readLine();
+		
+	}
+	String xml2String = sb.toString();
+        xml2String = xml2String.replaceAll("\\<.*?\\>", "");
+        bufReader.close();
+   
+        //String zahir=encodedString;
+        String zahir=encodedString+"."+encoded+"."+ xml2String+"."+encrypted;
+        
+        System.out.print(zahir);
+     	}else {
+     		System.out.println("Gabim: Celesi publik \'"+emri_i_perdoruesit+"\' nuk ekziston.");
+     	}
        
          
     }
+		
         
-    
-	    
-	    
-	 
- 
-}
+       
+         
+   
