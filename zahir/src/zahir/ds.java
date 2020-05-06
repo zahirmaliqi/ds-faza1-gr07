abef5088ef245effd1859164b110a534bb048c5d
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-05-06 23:13:01 +0200

    Update ds.java
    Komiti final

--- zahir/src/zahir/ds.java
+++ zahir/src/zahir/ds.java
@@ -5,20 +5,20 @@
 	       
 		 if(args.length==0){
 	            System.out.println("Nuk eshte dhene ndonje argument");
-	            System.exit(0);
+	            System.exit(1);
 	            }
 		      
 	        if (!args[0].equals("frequency")&&!args[0].equals("four-square")&&!args[0].equals("vigenere")&&!args[0].equals("create-user")
 		    &&!args[0].equals("delete-user")&&!args[0].equals("export-key")&&!args[0].equals("import-key")&&!args[0].equals("write-message") 
 		   &&!args[0].equals("read-message") ){
 	            System.out.println("Vlerat e lejuara jane [frequency,four-square,vigenere,create-user,delete-user,export-key,import-key,read-message,write-message]");
-	            System.exit(0);
+	            System.exit(1);
 	            }
 		      
 		if (args[0].equals("frequency")) {
                    if(args.length!=2 || args[1].equals("")){
                       System.out.println("Argumenti i dyte duhet te jete mesazhi me gjatesi>0");
-                      System.exit(0);
+                      System.exit(1);
                       }
                     frequency frequencyInspector = new frequency(args[1]);
                     frequencyInspector.fillHashMap();
@@ -28,11 +28,11 @@
 		if (args[0].equals("four-square")){
                   if(args.length!=5){
                        System.out.println("Argumentet encrypt|decrypt <key1> <key2> <plaintext|ciphertext>");
-                       System.exit(0);
+                       System.exit(1);
 		  }
                  if(!args[1].equals("encrypt")&&!args[1].equals("decrypt")){
                        System.out.println("Argumentet e lejuara  encrypt|decrypt");
-                       System.exit(0);
+                       System.exit(1);
                   }
             switch (args[1]){
                 case "encrypt":
@@ -50,11 +50,11 @@
 		     if (args[0].equals("vigenere")) {
 			if (args.length != 4) {
 			    System.out.println("Argumentet encrypt|decrypt <key> <plaintext|ciphertext>");
-			    System.exit(0);
+			    System.exit(1);
 			    }
 		         if (!args[1].equals("encrypt") && !args[1].equals("decrypt")) {
 			     System.out.println("Argumentet e lejuara  encrypt|decrypt");
-			     System.exit(0);
+			     System.exit(1);
 			     }
 			vigenere vigenere = new vigenere(args[2].toUpperCase(), args[3].toUpperCase());
 			switch (args[1]) {
@@ -70,11 +70,11 @@
 		   if (args[0].equals("create-user")) {
 	 		if (args.length != 2) {
 	 		    System.out.println("Argumentet emri");
-	 		    System.exit(0);
+	 		    System.exit(1);
 	 		    }
 			 if (args[1].contains(" ")) {
-			     System.out.println("Nuk lejohen hapesirat");
-			     System.exit(0);
+			     System.out.println("Nuk lejohen hapesirat.");
+			     System.exit(1);
 			    }
 			creat createt = new creat(args[1]);
 	 		createt.file();
@@ -83,16 +83,17 @@
 		      
 		      if (args[0].equals("delete-user")) {
 		 		if (args.length != 2) {
-		 		    System.out.println("Argumentet emri");
-		 		    System.exit(0);
+		 		    System.out.println("Argumentet emri.");
+		 		    System.exit(1);
 		 		    }
 			      if (args[1].contains(" ")) {
 				     System.out.println("Nuk lejohen hapesirat");
-				     System.exit(0);
+				     System.exit(1);
 			          }
 		 		delete del = new delete(args[1]);
 		 		del.delete_file();
 		 	   }
+		      
 		      
 		      if (args[0].equals("export-key")) {
 		 	 if (args.length == 3) {
@@ -105,7 +106,7 @@
 		               exc.mungon_argumentipriv();
 		               break;
 		           }
-		 	    System.exit(0);
+		 	    System.exit(1);
 		       }
 		        if (args.length == 4) {
 		 	       excport exc1=new excport(args[2],args[3]);
@@ -117,13 +118,13 @@
 		               exc1.ex_compriv();
 		               break;
 		               }
-		 		 System.exit(0);
+		 		 System.exit(1);
 		 	     }
 		 		
 		 	  if(args.length != 4 || args.length != 3)
 		 		{
 		 			System.out.println("Argumenti <public|private> <name> [file]");
-		 			System.exit(0);
+		 			System.exit(1);
 		 		}
 		        }
 		      
@@ -131,7 +132,7 @@
 		      if (args[0].equals("import-key")) {
 		 		if (args.length != 3) {
 		 		    System.out.println("Argumentet import-key <name> <path>");
-		 		    System.exit(0);
+		 		    System.exit(1);
 		 		    }
 			      
 			      if (args.length == 3) {
@@ -152,19 +153,19 @@
 	    		if (args.length == 3) {
 		 			writemessage Wm=new writemessage(args[1], args[2],null);
 		 			Wm.enc();
-		 		        System.exit(0);
+		 		        System.exit(1);
 		 		               }
 	    		
 	    		if (args.length == 4) {
 		 			writemessage Wm=new writemessage(args[1], args[2],args[3]);
 		 			Wm.ruaje();
-		 		        System.exit(0);
+		 		        System.exit(1);
 		 		                 }
 	    		
 	    		if(args.length != 4 || args.length != 3)
 		 		{
 		 			System.out.println("Argumenti <name> <message> [file]");
-		 			System.exit(0);
+		 			System.exit(1);
 		 		}
 		 	 }
 			      
@@ -177,12 +178,12 @@
 	    	             }else {
 	    	    	        read.decry();
 	    	              }
-	    	              System.exit(0);
+	    	              System.exit(1);
 		 	   }
 	    	     if(args.length != 2)
 		 		{
 		 			System.out.println("Argumenti <encrypted-message>");
-		 			System.exit(0);
+		 			System.exit(1);
 		 		}
 	            }
 		 		
