a92e9e8f905ce6241b3d947998880b844b9ecab2
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-06-04 22:22:05 +0200

    Update login.java
    Ketu kemi klasen qe e  krijon  tokenin kryesisht pjesen e payload i cili permbane  emrin dhe paswordin (paswordi ne token e kame ruajtur si hash e jo si pasword normal )

--- zahir/src/zahir/login.java
+++ zahir/src/zahir/login.java
@@ -1,42 +1,68 @@
 package zahir ;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.time.Instant;
 import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Base64;
 import java.util.Date;
-import java.util.Random;
+import java.util.Scanner;
 
-public class token1 {
-    public static void main(String[] args)
-    {
-        Instant now = Instant.now();
-        byte[] secret= Base64.getDecoder().decode("jZ3DUmVv4xYiw+V5V/1f6KrtSAWsb+bBKZbaZXnPWNbRTlEywYIC+Q9bwZg1tHWBJMt3jPfKQgoMT7G4wsvPV7I3tOW6Yr5X6p74pDZY8akQX7+8PUnzzwxwssj+LMek4nB2GSFTZdOe1p+OnA4ujko+yAFncnytI2wmFJ35UNkAQAB=");
-        String jwt=Jwts.builder()
-                .setSubject("zahir11")
-                .setAudience("paswor3d")
-                .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(20).toInstant()))
-                .signWith(Keys.hmacShaKeyFor(secret))
-                .compact();
-        System.out.println(jwt);
-         Jws<Claims> result= Jwts.parser()
-                 .setSigningKey(Keys.hmacShaKeyFor(secret))
-                 .parseClaimsJws(jwt);
+public class login {
+           private final String emri;
 
-
-
-         System.out.println(result);
-         System.out.println("Emri:"+result.getBody().getExpiration() +decodeJWT(jwt).getSubject());
-
-
+           public login(String emri) {
+                 this.emri=emri;
     }
- 
+    public void krijimi_i_tokenit() throws Exception {
+
+        File privat = new File("C:\\Users\\lenovo\\IdeaProjects\\jjwt-example\\out\\artifacts\\jjwt_example_jar\\"+emri+".xml");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Jep fjalkalimin: ");
+        String password = sc.nextLine();
+
+        if (privat.exists()){
+        String salt = "hash_algoritmi_dhe_salting";
+        String hash = md5(password + salt);
+
+        File myObj = new File("C:\\Users\\lenovo\\IdeaProjects\\jjwt-example\\out\\artifacts\\jjwt_example_jar\\"+emri+".txt");
+        Scanner myReader = new Scanner(myObj);
+            String hashi_i_pass = myReader.nextLine();
+
+
+
+
+        if( hash.equals(hashi_i_pass)) {
+            String celese_priv = getPrivKey(privat);
+            byte[] secret = Base64.getDecoder().decode(celese_priv);
+            Instant now = Instant.now();
+            String jwt = Jwts.builder()
+                    .setSubject(emri)
+                    //.setAudience(password)
+                    .setAudience(hashi_i_pass)
+                    .setIssuedAt(Date.from(now))
+                    .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(20).toInstant()))
+                    .signWith(Keys.hmacShaKeyFor(secret))
+                    .compact();
+                    System.out.println(jwt);
+            Jws<Claims> result = Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret))
+                    .parseClaimsJws(jwt);
+        } else{
+            System.out.println("Gabim: Shfrytezuesi ose fjalekalimi i gabuar.");
+                }
+        }
+  }
 
 }
