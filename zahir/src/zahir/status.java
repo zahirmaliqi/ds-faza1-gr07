e89bf0833349c6174fa414003a8890840e6e6bbe
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-06-05 16:44:33 +0200

    Update status.java
    Klasa e cila thirret ne rastin kur marrum te dhenat nga nje token .Te dhenat qe do te nxjerrim nga tokeni jane :korospoduesi i tokenit (User ) .Shikojme nese tokeni eshte valid sepse validushmeria e tij ne detyren tone eshte 20 min .Ku e kemi marr me ane  "getExpiration();"ku ruhet data e skademit dhe e krahasojme me daten current .

--- zahir/src/zahir/status.java
+++ zahir/src/zahir/status.java
@@ -1,1 +1,41 @@
+package zahir;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+
+public class status {
+    private final String tokeni;
+
+    public status(String tokeni) {
+        this.tokeni = tokeni;
+    }
+
+    public void info_rreth_tokenit() throws Exception {
+        decodeJWT(tokeni);
+        System.out.println( "User :"+decodeJWT(tokeni).getSubject());
+       Date d1=decodeJWT(tokeni).getExpiration();
+        java.util.Date date=new java.util.Date();
+        if(d1.compareTo(date) > 0) {
+            System.out.println("Valid: po");
+        }
+
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
+        String strDate = formatter.format(d1);
+        System.out.println("Skadimi:"+strDate);
+
+    }
+   
+
+}
